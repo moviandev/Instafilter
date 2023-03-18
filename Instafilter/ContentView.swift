54a6bf23c7187d7bee923f4682d6ff2a0101416e
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-17 23:12:48 -0300

    rollback

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,24 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
-    
     var body: some View {
         Text("Hello, world!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation = true
-            }
-            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Cancel", role: .cancel) {  }
-            } message: {
-                Text("Select a new color")
-            }
+            .padding()
     }
 }
 
