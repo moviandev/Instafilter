5d0bd2537883e6f6e375847d6ccde6a446e712e1
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-19 22:51:27 -0300

    rollback

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,52 +5,12 @@
 //  Created by Matheus Viana on 17/03/23.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import SwiftUI
 
-struct ContentView: View {
-    @State private var image: Image?
-    
+struct ContentView: View {  
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-        }
-        .onAppear(perform: loadImage)
-    }
-    
-    func loadImage() {
-        guard let inputImage = UIImage(named: "example") else { return }
-        
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.twirlDistortion()
-        currentFilter.inputImage = beginImage
-        
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-        }
-        
-        if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
-        }
-        
-        if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
-        }
-        
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        if let cgImg = context.createCGImage(outputImage, from: outputImage.extent) {
-            let uiImage = UIImage(cgImage: cgImg)
-            image = Image(uiImage: uiImage)
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
