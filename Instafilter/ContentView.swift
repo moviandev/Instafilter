4c968bbf681d259a4d63f304113ca2864d36fa83
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-19 22:44:49 -0300

    querying the filters

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -28,8 +28,23 @@ struct ContentView: View {
         
         let context = CIContext()
         let currentFilter = CIFilter.twirlDistortion()
-        
         currentFilter.inputImage = beginImage
+        
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+        }
+        
         currentFilter.radius = 500
         currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
         
