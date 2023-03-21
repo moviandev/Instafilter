3cd5e2a173b983abe1e624fa070dae375d7d1006
Matheus Viana <matheus_o_viana@hotmail.com>
2023-03-21 00:06:53 -0300

    Rollback

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,38 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingImagePicker = false
-    
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select photo") {
-                showingImagePicker = true
-            }
-            
-            Button("Save image") {
-                guard let inputImage = inputImage else { return }
-                
-                let imageSaver = ImageSaver()
-                imageSaver.writeToPhotoAlbum(image: inputImage)
-            }
-        }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker(image: $inputImage)
-        }
-        .onChange(of: inputImage) { _ in
-            loadImage()
-        }
-    }
-    
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        Text("Hello, world!")
+            .padding()
     }
 }
 
